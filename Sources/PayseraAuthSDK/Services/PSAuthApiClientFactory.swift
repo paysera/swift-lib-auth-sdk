6018593ec0f193c54a585e364c343f24eb375d60
Pattio <Pattio@users.noreply.github.com>
2021-01-20 18:03:36 +0200

    Remove PSTrustedSession

--- Sources/PayseraAuthSDK/Services/PSAuthApiClientFactory.swift
+++ Sources/PayseraAuthSDK/Services/PSAuthApiClientFactory.swift
@@ -4,7 +4,7 @@ import PayseraCommonSDK
 
 public class PSAuthApiClientFactory {
     public static func createAuthApiClient(logger: PSLoggerProtocol? = nil) -> PSAuthApiClient {
-        let trustedSession = PSTrustedSession(interceptor: nil, hosts: ["auth-api.paysera.com"])
-        return PSAuthApiClient(session: trustedSession, logger: logger)
+        let session = Session()
+        return PSAuthApiClient(session: session, logger: logger)
     }
 }
